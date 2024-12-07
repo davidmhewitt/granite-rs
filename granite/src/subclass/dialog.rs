28c381824b08b78eef45d78612888c95e1ca9a14
David Hewitt <davidmhewitt@gmail.com>
2024-12-07 21:11:27 +0900

    feat: implement relm4 container for Dialog

--- granite/src/subclass/dialog.rs
+++ granite/src/subclass/dialog.rs
@@ -5,3 +5,20 @@ use gtk::subclass::prelude::*;
 pub trait GraniteDialogImpl: DialogImpl {}
 
 unsafe impl<T: GraniteDialogImpl> IsSubclassable<T> for Dialog {}
+
+#[cfg(feature = "relm4")]
+mod relm4 {
+    use crate::Dialog;
+    use gtk::prelude::{BoxExt, DialogExt};
+    use relm4::{ContainerChild, RelmContainerExt};
+
+    impl ContainerChild for Dialog {
+        type Child = gtk::Widget;
+    }
+
+    impl RelmContainerExt for Dialog {
+        fn container_add(&self, widget: &impl AsRef<Self::Child>) {
+            self.content_area().append(widget.as_ref());
+        }
+    }
+}
